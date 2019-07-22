7cff95e62eecfbe88c84bb32fe35fc918d0d5f27
Aleksandra Boryka <aleksandra.boryka@gmail.com>
2019-07-23 00:46:20 +0200

    Add information about user to moviesToWatch component

--- src/app/movies-to-watch/movies-to-watch.component.ts
+++ src/app/movies-to-watch/movies-to-watch.component.ts
@@ -1,24 +1,30 @@
+import { AngularFireAuth } from '@angular/fire/auth';
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-movies-to-watch',
   templateUrl: './movies-to-watch.component.html',
   styleUrls: ['./movies-to-watch.component.css']
 })
-export class MoviesToWatchComponent implements OnInit {
+export class MoviesToWatchComponent {
 
+  user: User;
   moviesToWatch = [];
-  constructor(private moviesService: MoviesService) {
+  constructor(private moviesService: MoviesService, private angularFire: AngularFireAuth) {
     this.moviesService.getMoviesToWatchObs().subscribe((movies: Array<any>) => {
       this.moviesToWatch = movies;
     })
+
+    angularFire.authState.subscribe(user => {
+      this.user = user;
+    })
   }
+
 
   removeFromWatchList(movie: Array<any>): void {
     this.moviesService.removeFromWatchList(movie);
-  }
-  ngOnInit() {
   }
 
 }
