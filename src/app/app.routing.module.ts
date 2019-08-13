25f428e904464f34181ac8f846257dff9e3d02e8
Aleksandra Boryka <aleksandra.boryka@gmail.com>
2019-08-13 22:54:47 +0200

    Bug fixing with routing

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,28 +1,14 @@
-import { MoviesComponent } from './movies/movies.component';
-import { AuthComponent } from './auth/auth.component';
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MoviesRoutingModule } from './movies/movies.routing.module';
+import { AuthRoutingModule } from './auth/auth.routing.module';
 
 
 const appRoutes: Routes = [
-  {
-    path: "repertuar",
-    component: MoviesComponent
-  },
-  {
-    path: "logowanie",
-    component: AuthComponent
-  },
-  {
-    path: "",
-    redirectTo: "/repertuar",
-    pathMatch: "full"
-  }
 ];
 
 @NgModule({
-  imports: [MoviesRoutingModule, RouterModule.forRoot(appRoutes)],
+  imports: [AuthRoutingModule, MoviesRoutingModule, RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
